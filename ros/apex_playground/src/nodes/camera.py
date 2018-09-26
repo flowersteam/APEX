44c296221e868b996bd8ec06575a6ec4ceb9cbf9
apere <alexandre.pere@inria.fr>
2018-09-26 10:52:35 +0200

    Adds cleanup

--- ros/apex_playground/src/nodes/camera.py
+++ ros/apex_playground/src/nodes/camera.py
@@ -13,22 +13,29 @@ class CameraService(object):
     def __init__(self, height, width):
         camera = cv2.VideoCapture(0)
         camera.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, height)
-	camera.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, width)
+	    camera.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, width)
         camera.set(cv2.cv.CV_CAP_PROP_FPS, 30)
         self.frame = np.zeros(1)
         self.frame_lock = Lock()
-        self.thread = Thread(target=self.__in_thread_loop, args=(camera, self.frame_lock))
+        self.exit = False
+        self.exit_lock = Lock()
+        self.thread = Thread(target=self.__in_thread_loop, args=(camera,))
         self.thread.start()
-	rospy.Service("camera", Camera, self.read)
+	    rospy.Service("camera", Camera, self.read)
 
-    def __in_thread_loop(self, camera, frame_lock):
+    def __in_thread_loop(self, camera):
         while True:
+            with self.exit_lock:
+                do_exit = self.exit
             success, frame = camera.read()
+            if do_exit:
+                camera.release()
+                break
             if not success:
                 rospy.logerr("Frame acquire failed")
                 continue
             else:
-                with frame_lock:
+                with self.frame_lock:
                     self.frame = frame
                 time.sleep(1/20)
 
@@ -40,11 +47,12 @@ class CameraService(object):
         return resp
 
     def close(self):
-        # cleanup the camera and close any open windows
-        self.camera.release()
-        cv2.destroyAllWindows()
+        with self.exit_lock:
+            self.exit = True
+        self.thread.join()
 
 if __name__=="__main__":
     rospy.init_node("camera")
     camera = CameraService(352, 288)
     rospy.spin() 
+    camera.close()
