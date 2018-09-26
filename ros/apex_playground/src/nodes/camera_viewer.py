ccd1c554830545edfe813b5f2fc1c00485c6e857
apere <alexandre.pere@inria.fr>
2018-09-26 14:09:49 +0200

    Update Camera_viewer

--- ros/apex_playground/src/nodes/camera_viewer.py
+++ ros/apex_playground/src/nodes/camera_viewer.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from rospkg import RosPack
 from apex_playground.srv import Camera, CameraRequest
 import json
+import os
 from os.path import join
 import numpy as np
 
@@ -14,13 +15,11 @@ import numpy as np
 class CameraViewer(object):
     def __init__(self):
         self.rospack = RosPack()
-        with open(join(self.rospack.get_path('apex_playground'), 'config', 'ergo.json')) as f:
-            self.params = json.load(f)
-        print(self.params)
+        self.apex_name = os.environ.get("ROS_HOSTNAME").replace("-ergo.local", "");
         
     def show_image(self):
-        rospy.wait_for_service('/apex_1/camera')
-        read = rospy.ServiceProxy('/apex_1/camera', Camera)
+        rospy.wait_for_service('/{}/camera'.format(self.apex_name))
+        read = rospy.ServiceProxy('/{}/camera'.format(self.apex_name), Camera)
         image = [x.data for x in read(CameraRequest()).image]
         image = np.array(image).reshape(144,176,3)
 	    plt.imshow(image)
