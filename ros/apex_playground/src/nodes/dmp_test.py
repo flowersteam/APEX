b2d565b6b6f240a7fb08f61c08fe42d59be5afbe
AdrienF <l.neirda@gmail.com>
2018-09-27 16:54:47 +0200

    Test bounds on ergo

--- ros/apex_playground/src/nodes/dmp_test.py
+++ ros/apex_playground/src/nodes/dmp_test.py
@@ -30,8 +30,8 @@ if __name__ == "__main__":
     n_bfs = 7
     timesteps = 30
     max_params = np.array([300.] * n_bfs * n_dmps + [1.] * n_dmps)
-    bounds_motors_max = 20 * np.ones(6)
-    bounds_motors_min = -20 * np.ones(6)
+    bounds_motors_max = np.array([180, 10, 20, 10, 30, 30])
+    bounds_motors_min = np.array([-180, -50, -50, -15, -50, -50])
     dmp = MyDMP(n_dmps=n_dmps, n_bfs=n_bfs, timesteps=timesteps, max_params=max_params)
 
     mover = ErgoDMP()
