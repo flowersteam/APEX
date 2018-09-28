bf60de6948661ae2b3637350e83d2991ee70fbeb
AdrienF <l.neirda@gmail.com>
2018-09-28 15:34:36 +0200

    Debugs image generator

--- ros/apex_playground/src/nodes/image_generator.py
+++ ros/apex_playground/src/nodes/image_generator.py
@@ -24,6 +24,7 @@ class CameraRecorder(object):
         rospy.wait_for_service('/{}/camera'.format(self.apex_name))
         read = rospy.ServiceProxy('/{}/camera'.format(self.apex_name), Camera)
         image = [x.data for x in read(CameraRequest()).image]
+        image = np.array(image).reshape(144, 176, 3)
         return image
 
 
