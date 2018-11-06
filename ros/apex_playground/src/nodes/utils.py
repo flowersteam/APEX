7525816370e30cf0a2adb0f619ac5b1007644691
AdrienF <l.neirda@gmail.com>
2018-11-06 15:00:54 +0100

    Debugs

--- ros/apex_playground/src/nodes/utils.py
+++ ros/apex_playground/src/nodes/utils.py
@@ -18,6 +18,130 @@ class BallTracking(object):
         self.pts = {}
         self.dX, self.dY = (0, 0)
 
+        self.camera = None
+
+    def read(self):
+        if not self.camera:
+            return False, None
+        success, image = self.camera.read()
+        return success, image[16:-16,48:-48]
+
+    def get_images(self, frame):
+        # resize the frame, blur it, and convert it to the HSV color space
+        #frame = imutils.resize(frame, width=600)
+        # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        mask_ball = cv2.inRange(hsv, self.params['tracking']['ball']['lower'], self.params['tracking']['ball']['upper'])
+        mask_ball = cv2.erode(mask_ball, None, iterations=4)
+        mask_ball = cv2.dilate(mask_ball, None, iterations=10)
+
+        mask_arena = cv2.inRange(hsv, self.params['tracking']['arena']['lower'], self.params['tracking']['arena']['upper'])
+        mask_arena = cv2.erode(mask_arena, None, iterations=4)
+        mask_arena = cv2.dilate(mask_arena, None, iterations=10)
+
+        return hsv, mask_ball, mask_arena
+
+    def find_center(self, name, frame, mask, min_radius):
+        if name not in self.pts:
+            self.pts[name] = deque(maxlen=self.params['tracking']['buffer_size'])
+
+        # find contours in the mask and initialize the current (x, y) center of the ball
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+
+        # only proceed if at least one contour was found
+        if len(cnts) > 0:
+            # find the largest contour in the mask, then use it to compute the minimum enclosing circle and centroid
+            c = max(cnts, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            center = (int(x), int(y))
+
+            # only proceed if the radius meets a minimum size
+            if radius > min_radius:
+                # draw the circle and centroid on the frame, then update the list of tracked points
+                cv2.circle(frame, center, int(radius), (0, 255, 255), 2)
+                cv2.circle(frame, center, 5, (0, 0, 255), -1)
+                self.pts[name].appendleft(center)
+                smooth_points = 8
+                return (int(np.mean([self.pts[name][i][0] for i in range(min(smooth_points, len(self.pts[name])))])),
+                        int(np.mean([self.pts[name][i][1] for i in range(min(smooth_points, len(self.pts[name])))]))), radius
+        return None, None
+
+    def draw_images(self, frame, hsv, mask_ball, mask_arena, arena_center, arena_ring_radius=None):
+        self.draw_history(frame, 'ball')
+        self.draw_history(frame, 'arena')
+        if arena_ring_radius is not None:
+            cv2.circle(frame, arena_center, arena_ring_radius, (0, 128, 255), 2)
+        return frame
+
+        #rgbs = cv2.split(frame)
+        #hsvs = cv2.split(hsv)
+
+        #cv2.imshow("Hue", hsvs[0])
+        #cv2.imshow("Mask ball", mask_ball)
+        #cv2.imshow("Mask arena", mask_arena)
+        #cv2.imshow("Frame", frame)
+        #cv2.waitKey(1)
+
+        #cv2.imshow("Red", rgbs[0])
+        #cv2.imshow("Green", rgbs[1])
+        #cv2.imshow("Blue", rgbs[2])
+        #cv2.imshow("Saturation", hsvs[1])
+        #cv2.imshow("Value", hsvs[2])
+        #cv2.waitKey(1)
+
+    def draw_history(self, frame, name):
+        # loop over the set of tracked points
+        if name in self.pts:
+            for i in np.arange(1, len(self.pts[name])):
+                # if either of the tracked points are None, ignore
+                # them
+                if self.pts[name][i - 1] is None or self.pts[name][i] is None:
+                    continue
+
+                # check to see if enough points have been accumulated in
+                # the buffer
+                if len(self.pts[name]) >= 10 and i == 1 and self.pts[name][-10] is not None:
+                    # compute the difference between the x and y
+                    # coordinates and re-initialize the direction
+                    # text variables
+                    self.dX = self.pts[name][-10][0] - self.pts[name][i][0]
+                    self.dY = self.pts[name][-10][1] - self.pts[name][i][1]
+
+                # otherwise, compute the thickness of the line and
+                # draw the connecting lines
+                thickness = int(np.sqrt(self.params['tracking']['buffer_size'] / float(i + 1)) * 2.5)
+                cv2.line(frame, self.pts[name][i - 1], self.pts[name][i], (0, 0, 255), thickness)
+            if len(self.pts[name]) > 1:
+                # show the movement deltas of movement
+                cv2.putText(frame, "dx: {}, dy: {}".format(self.dX, self.dY),
+                            (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                            0.35, (0, 0, 255), 1)
+
+    def open(self, width=800, height=600):
+        try:
+            self.camera = cv2.VideoCapture(0)
+        except:
+            return False
+        else:
+            self.camera.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, height)
+            self.camera.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, width)
+            return True
+
+    def close(self):
+        # cleanup the camera and close any open windows
+        self.camera.release()
+        cv2.destroyAllWindows()
+
+
+class MyBallTracking(object):
+    def __init__(self, parameters):
+        self.params = parameters
+
+        # initialize the lists of tracked points in a map and the coordinate deltas
+        self.pts = {}
+        self.dX, self.dY = (0, 0)
+
     def get_images(self, frame):
         frame = frame[16:-16, 48:-48]
         # resize the frame, blur it, and convert it to the HSV color space
@@ -154,15 +278,15 @@ if __name__ == "__main__":
     from rospkg import RosPack
     import time
 
-    camera = CameraRecorder(1)
-    image = camera.get_image()
-    plt.imshow(image)
-    plt.show()
-    time.sleep(2)
-    plt.close('all')
-
-    mover = ErgoMover(1)
-    mover.set_compliant(False)
+    # camera = CameraRecorder(1)
+    # image = camera.get_image()
+    # plt.imshow(image)
+    # plt.show()
+    # time.sleep(2)
+    # plt.close('all')
+    #
+    # mover = ErgoMover(1)
+    # mover.set_compliant(False)
 
     rospack = RosPack()
     with open(join(rospack.get_path('apex_playground'), 'config', 'environment.json')) as f:
@@ -173,9 +297,7 @@ if __name__ == "__main__":
     params['tracking']['arena']['upper'] = tuple(params['tracking']['arena']['upper'])
 
     tracking = BallTracking(params)
-
-    frame = camera.get_image()
-    img = frame.copy().reshape(144, 176, 3)
+    grabbed, frame = tracking.read()
 
     hsv, mask_ball, mask_arena = tracking.get_images(frame)
 
