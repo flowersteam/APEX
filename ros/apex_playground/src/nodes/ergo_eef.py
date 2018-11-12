609e698b609aec15d987018505af3834c3f45c41
AdrienF <l.neirda@gmail.com>
2018-11-12 18:22:25 +0100

    Debugs ergo_eef node

--- ros/apex_playground/src/nodes/ergo_eef.py
+++ ros/apex_playground/src/nodes/ergo_eef.py
@@ -10,20 +10,8 @@ class ErgoEefPos(object):
         self.sub = rospy.Subscriber("/apex_1/poppy_ergo_jr/end_effector_pose", PoseStamped, self.cb_eef)
 
     def cb_eef(self, msg):
-        rospy.loginfo(rospy.get_caller_id() + "I heard %s", msg)
+        rospy.loginfo(rospy.get_caller_id() + "I heard %s", msg.pose.position)
         self.cur_eef_pos = msg
-
-    def run(self):
-        # rospy.wait_for_service(service)
-        # self.set_torso_compliant_srv = rospy.ServiceProxy(self.service_name_set_compliant, SetTorsoCompliant)
-        # rospy.Service(self.service_name_get, GetSensorialState, self.cb_get)
-        rospy.loginfo("Done, perception is up!")
-        rospy.Subscriber("/apex_1/poppy_ergo_jr/end_effector_pose", PoseStamped, self.cb_eef)
-        rospy.loginfo("Done, perception is down!")
-        rospy.spin()
-
-    # def get(self):
-    #    return self.eef_pose
 
     @property
     def eef_pos(self):
@@ -48,5 +36,3 @@ if __name__ == '__main__':
     rospy.init_node('ergoeff')
     ergo = ErgoEefService()
     ergo.run()
-    # print(ergo.ergo)
-    # ErgoEefService().run()
