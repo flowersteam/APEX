34faf4712f1c60f4fe441f0b9d5ce5bc3706f84f
AdrienF <l.neirda@gmail.com>
2018-11-12 17:18:10 +0100

    Debugs ergo_eef node

--- ros/apex_playground/src/nodes/ergo_eef.py
+++ ros/apex_playground/src/nodes/ergo_eef.py
@@ -9,7 +9,7 @@ class ErgoEefPos(object):
         # self.rospack = RosPack()
         # print("Ego Eff Pose on {}".format(n_apex))
         # rospy.Subscriber(self.topics["torso_l_eef"]["topic"], self.topics["torso_l_eef"]["type"], self.cb_eef)
-        self.sub = rospy.Subscriber("poppy_ergo_jr/end_effector_pose", PoseStamped, self.cb_eef)
+        # self.sub = rospy.Subscriber("poppy_ergo_jr/end_effector_pose", PoseStamped, self.cb_eef)
 
     def cb_eef(self, msg):
         rospy.loginfo(rospy.get_caller_id() + "I heard %s", msg)
@@ -20,6 +20,8 @@ class ErgoEefPos(object):
         # self.set_torso_compliant_srv = rospy.ServiceProxy(self.service_name_set_compliant, SetTorsoCompliant)
         # rospy.Service(self.service_name_get, GetSensorialState, self.cb_get)
         rospy.loginfo("Done, perception is up!")
+        rospy.Subscriber("poppy_ergo_jr/end_effector_pose", PoseStamped, self.cb_eef)
+        rospy.loginfo("Done, perception is down!")
         rospy.spin()
 
     # def get(self):
