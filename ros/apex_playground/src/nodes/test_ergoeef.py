0905f252116aa5e710a6a8a82f383c511c3a609b
AdrienF <l.neirda@gmail.com>
2018-11-12 19:08:53 +0100

    Debugs ergo_eef node

--- ros/apex_playground/src/nodes/test_ergoeef.py
+++ ros/apex_playground/src/nodes/test_ergoeef.py
@@ -7,11 +7,11 @@ class ErgoPos(object):
     def __init__(self, n_apex):
         self.apex_name = "apex_{}".format(n_apex)
         rospy.wait_for_service('/{}/ergoeff'.format(self.apex_name))
-        self.pos = rospy.ServiceProxy('/{}/ergoeff'.format(self.apex_name), ErgoPose)
+        self.get_msg = rospy.ServiceProxy('/{}/ergoeff'.format(self.apex_name), ErgoPose)
         print("ErgoPose on ", self.apex_name)
 
-    def get_pos(self):
-        msg = self.pos()
+    def get_position(self):
+        msg = self.get_msg()
         # msg = self.get_pos()
         pos = np.array([msg.pose.position.x, msg.pose.position.y, msg.pose.position.z])
         print(pos)
@@ -19,4 +19,4 @@ class ErgoPos(object):
 
 if __name__ == "__main__":
     ergo_pos = ErgoPos(1)
-    ergo_pos.get_pos()
+    ergo_pos.get_position()
