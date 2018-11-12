f043969bc1de8e9033ff91185de08f3db19826e1
AdrienF <l.neirda@gmail.com>
2018-11-12 15:35:15 +0100

    Debugs test_ergoeef

--- ros/apex_playground/src/nodes/test_ergoeef.py
+++ ros/apex_playground/src/nodes/test_ergoeef.py
@@ -1,6 +1,7 @@
 import rospy
 from rospkg import RosPack
 from geometry_msgs.msg import PoseStamped
+import time
 
 
 class ErgoEefPose(object):
@@ -9,10 +10,17 @@ class ErgoEefPose(object):
         # self.rospack = RosPack()
         print("Ego Eff Pose on {}".format(n_apex))
         # rospy.Subscriber(self.topics["torso_l_eef"]["topic"], self.topics["torso_l_eef"]["type"], self.cb_eef)
-        self.sub = rospy.Subscriber("{}/end_effector_pose".format(n_apex), PoseStamped, self.cb_eef)
+        self.sub = rospy.Subscriber("/{}/poppy_ergo_jr/end_effector_pose".format(n_apex), PoseStamped, self.cb_eef)
 
     def cb_eef(self, msg):
         self.eef_pose = msg
+
+    def run(self):
+        # rospy.wait_for_service(service)
+        # self.set_torso_compliant_srv = rospy.ServiceProxy(self.service_name_set_compliant, SetTorsoCompliant)
+        # rospy.Service(self.service_name_get, GetSensorialState, self.cb_get)
+        rospy.loginfo("Done, perception is up!")
+        rospy.spin()
 
     @property
     def ergo(self):
@@ -23,4 +31,5 @@ class ErgoEefPose(object):
 
 if __name__ == '__main__':
     ergo_eff_pose = ErgoEefPose(1)
+    time.sleep(5)
     print(ergo_eff_pose.ergo)
