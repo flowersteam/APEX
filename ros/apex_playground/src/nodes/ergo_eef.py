3934c136041eaeb5626de056f0edc5b7440eeb8f
AdrienF <l.neirda@gmail.com>
2018-11-12 18:38:48 +0100

    Debugs ergo_eef node

--- ros/apex_playground/src/nodes/ergo_eef.py
+++ ros/apex_playground/src/nodes/ergo_eef.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python
+import numpy as np
+
 import rospy
 from geometry_msgs.msg import PoseStamped
 from apex_playground.srv import ErgoPose, ErgoPoseResponse
 
-import numpy as np
-
 
 class ErgoEefPos(object):
     def __init__(self):
-        self.apex_name = "apex_1"
-        self.sub = rospy.Subscriber("/apex_1/poppy_ergo_jr/end_effector_pose", PoseStamped, self.cb_eef)
+        self.sub = rospy.Subscriber("poppy_ergo_jr/end_effector_pose", PoseStamped, self.cb_eef)
 
     def cb_eef(self, msg):
-        # rospy.loginfo(rospy.get_caller_id() + "I heard %s", msg.pose.position)
-        print(np.array([msg.pose.position.x, msg.pose.position.y, msg.pose.position.z]))
         self.cur_eef_pos = msg
 
     @property
@@ -26,9 +23,8 @@ class ErgoEefService(object):
         self.ergo_pos = ErgoEefPos()
 
     def run(self):
-        rospy.loginfo("Perception is down!")
         rospy.Service("ergoeff", ErgoPose, self.get_pos)
-        rospy.loginfo("Done, perception is up!")
+        rospy.loginfo("Done, end effector position perception is up!")
         rospy.spin()
 
     def get_pos(self):
--- ros/apex_playground/src/nodes/test_ergoeef.py
+++ ros/apex_playground/src/nodes/test_ergoeef.py
@@ -1,28 +1,20 @@
 import rospy
-from geometry_msgs.msg import PoseStamped
 from apex_playground.srv import ErgoPose, ErgoPoseRequest
 
 
 class ErgoPos(object):
     def __init__(self, n_apex):
         self.apex_name = "apex_{}".format(n_apex)
+        rospy.wait_for_service('/{}/ergoeff'.format(self.apex_name))
+        self.get_pos = rospy.ServiceProxy('/{}/ergoeff'.format(self.apex_name), ErgoPose)
         print("ErgoPose on ", self.apex_name)
 
-    def get_pose(self):
-        rospy.wait_for_service('/{}/ergoeff'.format(self.apex_name))
+    def get_pos(self):
+        msg = self.get_pos(ErgoPoseRequest())
+        pos = np.array([msg.pose.position.x, msg.pose.position.y, msg.pose.position.z])
+        print(pos)
 
-
-        read = rospy.ServiceProxy('/{}/ergoeff'.format(self.apex_name), Camera)
-        image = [x.data for x in read(CameraRequest()).image]
-        image = np.array(image).reshape(144, 176, 3)
-        plt.imshow(image)
-        plt.show()
 
 if __name__ == "__main__":
-    camera = CameraViewer()
-    camera.show_image()
-
-
-if __name__ == '__main__':
-    rospy.init_node('ergoeff')
-    ErgoEefPose().run()
+    camera = ErgoPos(1)
+    camera.get_pos()
